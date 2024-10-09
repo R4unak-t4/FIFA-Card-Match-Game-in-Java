642b6bde0a6c127e45e09ae128bc5c20b01a0cda
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-09 19:54:27 +0545

    Initialization

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.awt.*;
+import java.awt.event.*;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import javax.swing.*;
+public class App {
+    class Card{
+        String cardNme;
+        ImageIcon cardIcon;
+
+        Card(String cardNme, ImageIcon cardIcon) {
+            this.cardNme = cardNme;
+            this.cardIcon = cardIcon;
+        }
+
+        public String toString(){
+            return cardNme;
+        }
+    }
+    String [] cardList = {
+            "Dimaria","Dybala","fornlan","griezman","Haland","maldini",
+            "Messi","Neymr","Ronldo","Vanbasten"
+    };
+    int rows = 4;
+    int col = 5;
+    int cardWidth = 90;
+    int cardHeight = 120;
+
+    Array
+
+    public void MatchCard() {
+        System.out.println("Card Match");
+    }
+}
